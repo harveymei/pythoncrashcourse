1f8058d6c9b7a04ab485f78ce54ac354810036a7
Harvey Mei <harvey.mei@msn.com>
2020-11-30 10:56:09 +0800

    测试多个条件

--- toppings.py
+++ toppings.py
@@ -116,3 +116,57 @@ elif age >= 65:
 print("Your new admission cost is $" + str(price) + ".")
 
 # 5.3.6 测试多个条件
+# if-elif-else仅适用于只有一个条件满足的情况
+requested_toppings = ['mushrooms', 'extra cheese']
+
+if 'mushrooms' in requested_toppings:
+    print("Adding mushrooms.")
+if 'pepperoni' in requested_toppings:  # 不存在，因此不会打印输出
+    print("Adding pepperoni.")
+if 'extra cheese' in requested_toppings:
+    print("Adding extra cheese.")
+
+print("\nFinished making your pizza!")
+
+# 使用if语句处理列表
+# 检查特殊元素
+requested_toppings = ['mushrooms', 'green peppers', 'extra cheese']
+
+for requested_topping in requested_toppings:
+    print("Adding " + requested_topping + ".")
+
+print("\nFinished making your pizza!")
+
+# 使用for循环中包含if语句，处理一种材料用完的情况
+requested_toppings = ['mushrooms', 'green peppers', 'extra cheese']
+
+for requested_topping in requested_toppings:
+    if requested_topping == 'green peppers':
+        print("Sorry, we are out of green peppers right now.")
+    else:
+        print("Adding " + requested_topping + ".")
+
+print("\nFinished making your pizza!")
+
+# 确定列表不是空的
+requested_toppings = []
+
+if requested_toppings:  # requested_topping = True 列表不为空
+    for requested_topping in requested_toppings:
+        print("Adding " + requested_topping + ".")
+    print("\nFinished making your pizza!")
+else:  # requested_topping = False 列表为空
+    print("Are you sure you want a plain pizza?")
+
+
+# 使用多个列表
+available_toppings = ['mushrooms', 'olives', 'green peppers',
+                      'pepperoni', 'pineapple', 'extra cheese']
+requested_toppings = ['mushrooms', 'french fries', 'extra cheese']
+# 使用for循环获取列表中的元素并使用if语句判断该元素是否存在于另外一个列表中
+for requested_topping in requested_toppings:
+    if requested_topping in available_toppings:
+        print("Adding " + requested_topping + ".")
+    else:
+        print("Sorry, we don't have " + requested_topping + ".")
+    print("\nFinished making your pizza!")
